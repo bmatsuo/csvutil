f37d0f396dc336e1762bb87eca282578957cb0d7
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-22 07:56:49 -0700

    Remove old config stuff from csv.go

--- csv.go
+++ csv.go
@@ -2,15 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//  CSV data utilities for the Go programming language.
+//  Package csvutils provides formatted reading and writing of CSV data.
 package csvutil
 import ()
-
-//  The default CSV field separator is a comma ','. By default, field
-//  trimming is turned off for csvutil's I/O objects. But, when active,
-//  the default cutset is " \t", that is, (ASCII) whitespace.
-const (
-    DefaultSep    = ','
-    DefaultTrim   = false
-    DefaultCutset = " \t"
-)
