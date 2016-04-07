9dfce04aa256787c17b425215606c0160a8d4e2d
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-04-07 06:44:37 -0700

    clearly state in the go docs that the package is unmaintained

--- csvutil.go
+++ csvutil.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//  Package csvutils provides formatted reading and writing of CSV data.
+//  Package csvutil is not maintained.  It provides formatted reading and writing of CSV data.
 package csvutil
 
 import ()
